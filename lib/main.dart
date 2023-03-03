e60d05cc1fc70ae891ec0b12b745a861dd588ad0
WireduKwabena <wiredukwabena@outlook.com>
2023-03-03 19:24:45 +0000

    Another commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+    
   runApp(
     GetMaterialApp(
       title: "Application",
